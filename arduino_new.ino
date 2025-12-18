7c5a393462131876c7838c179cfc0a78eb8d6b76
Ivan Sidorov <nobxod@gmail.com>
2025-12-18 18:00:56 +0300

    up

--- arduino_new.ino
+++ arduino_new.ino
@@ -6,125 +6,119 @@ uint8_t ggg2 = 34;
 String ggg3 = "hello!";
 
 
-enum class XxxType {
-    INT8,
-    UINT8,
-    INT16,
-    UINT16,
-    STRING,
-};
-
 
 struct XxxParam {
     PGM_P name;
     void* var;
-    XxxType type;
-
-    String getValue() {
-        switch (type) {
-            case XxxType::INT8:
-                return String(*(int8_t*)var);
-            case XxxType::UINT8:
-                return String(*(uint8_t*)var);
-            case XxxType::INT16:
-                return String(*(int16_t*)var);
-            case XxxType::UINT16:
-                return String(*(uint16_t*)var);
-            case XxxType::STRING:
-                return *(String*)var;
-        }
-    }
+    String (*getValue)(void* var);
 };
 
 
-template <uint8_t num>
-class XxxParamsCollection {
-private:
-    XxxParam params[num];
-    uint8_t count = 0;
-
-public:
-    void add(XxxParam param) {
-        params[count] = param;
-        ++count;
-    }
-
-    bool notEmpty() {
-        return count > 0;
-    }
-
-    void foreach(void (*fn)(XxxParam&)) {
-        uint8_t i;
-        for (i = 0; i < count; i++) {
-            fn(params[i]);
-        }
-    }
-};
+String getValue_INT8(void* var) {
+    return String(*(int8_t*)var);
+}
+constexpr XxxParam buildXxxParam(PGM_P name, int8_t* var) {
+    return {name, var, getValue_INT8};
+}
 
 
-template <uint8_t num>
+String getValue_UINT8(void* var) {
+    return String(*(uint8_t*)var);
+}
+constexpr XxxParam buildXxxParam(PGM_P name, uint8_t* var) {
+    return {name, var, getValue_UINT8};
+}
+
+
+String getValue_INT16(void* var) {
+    return String(*(int16_t*)var);
+}
+constexpr XxxParam buildXxxParam(PGM_P name, int16_t* var) {
+    return {name, var, getValue_INT16};
+}
+
+
+String getValue_UINT16(void* var) {
+    return String(*(uint16_t*)var);
+}
+constexpr XxxParam buildXxxParam(PGM_P name, uint16_t* var) {
+    return {name, var, getValue_UINT16};
+}
+
+
+String getValue_STRING(void* var) {
+    return String(*(String*)var);
+}
+constexpr XxxParam buildXxxParam(PGM_P name, String* var) {
+    return {name, var, getValue_STRING};
+}
+
+
+
 class XXX {
 private:
-    XxxParamsCollection<num> params;
+    const XxxParam** params;
 
 public:
-    void setParam(PGM_P name, int8_t* var) {
-        XxxParam param = {name, var, XxxType::INT8};
-        params.add(param);
-    }
-
-    void setParam(PGM_P name, uint8_t* var) {
-        XxxParam param = {name, var, XxxType::UINT8};
-        params.add(param);
-    }
-
-    void setParam(PGM_P name, int16_t* var) {
-        XxxParam param = {name, var, XxxType::INT16};
-        params.add(param);
-    }
-
-    void setParam(PGM_P name, uint16_t* var) {
-        XxxParam param = {name, var, XxxType::UINT16};
-        params.add(param);
-    }
-
-    void setParam(PGM_P name, String* var) {
-        XxxParam param = {name, var, XxxType::STRING};
-        params.add(param);
+    XXX(const XxxParam** params) {
+        this->params = params;
     }
 
     void update() {
         if (Serial.available()) {
-            char* line = Serial.readStringUntil('\n').c_str();
-            if (strcmp_P(line, PSTR("help")) == 0) {
+            String line = Serial.readStringUntil('\n');
+            if (strcmp_P(line.c_str(), PSTR("help")) == 0) {
                 cmdHelp();
             }
         }
     }
 
     void cmdHelp() {
-        if (params.notEmpty()) {
-            params.foreach([](XxxParam& param) {
-                Serial.print(FPSTR(param.name));
-                Serial.print(" ");
-                Serial.print(param.getValue());
-                Serial.println();
-            });
-        } else {
-            Serial.println(F("Hello"));
+        uint8_t i;
+        XxxParam param;
+        for (i = 0; ; ++i) {
+            XxxParam* param_p = pgm_read_ptr(&params[i]);
+            if (!param_p) break;
+            memcpy_P(&param, param_p, sizeof(XxxParam));
+            printParam(param);
         }
+        if (!i) {
+            Serial.println(F("Not has params!"));
+        }
+    }
+
+    void printParam(const XxxParam& param) {
+        Serial.print(FPSTR(param.name));
+        Serial.print(" ");
+        Serial.print(param.getValue(param.var));
+        Serial.println();
     }
 };
 
 
-XXX<3> myXxx;
+
+
+const char ggg1_name[] PROGMEM = "ggg1";
+const XxxParam ggg1_param PROGMEM = buildXxxParam(ggg1_name, &ggg1);
+
+const char ggg2_name[] PROGMEM = "ggg2";
+const XxxParam ggg2_param PROGMEM = buildXxxParam(ggg2_name, &ggg2);
+
+const char ggg3_name[] PROGMEM = "ggg3";
+const XxxParam ggg3_param PROGMEM = buildXxxParam(ggg3_name, &ggg3);
+
+const XxxParam* const params_X[] PROGMEM = {
+    &ggg1_param,
+    &ggg2_param,
+    &ggg3_param,
+    nullptr,
+};
+
+XXX myXxx(params_X);
 
 
 void setup() {
     Serial.begin(9600);
-    myXxx.setParam(PSTR("ggg1"), &ggg1);
-    myXxx.setParam(PSTR("ggg2"), &ggg2);
-    myXxx.setParam(PSTR("ggg3"), &ggg3);
 }
 
 
